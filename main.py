3b2eb0f43715b88de9c6d70a02d91bb5a80ce27c
Vamsi Krishna <vamsikrishna@Mac-140.lan>
2026-02-28 13:39:47 -0600

    environment setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+
+# import os
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!")
+    # print(os.environ.get("OPENAI_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
